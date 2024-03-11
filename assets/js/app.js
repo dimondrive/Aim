b63bbaeefaf968a61f6a595b592eff07bc0a926c
дмитрий василевский <dmitrijvasilevskij@MacBook-Air-dmitrij.local>
2024-03-11 14:34:05 +0300

    added:Timer

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 const screens = document.querySelectorAll('.screen');
 const screen__start = document.querySelector('.screen__start');
 const timeList = document.querySelector('.time-list')
-
+const resultTime = document.querySelector('.result');
+const times = document.querySelector('.time');
 
 let time = 0;
 
@@ -21,4 +22,20 @@ timeList.addEventListener('click',(e)=>{
 })
 function startGame(){
     screens[1].classList.add('up');
+    setInterval(startTimer,1000);
+    times.innerHTML=`00:${time}`;
+}
+
+function startTimer(){
+    if(time==0){
+        resultTime.innerHTML=`Время вышло...`
+    }
+    else{
+        let current = --time;
+        if(current<10){
+            current=`0${current}`;
+            console.log(time);
+        }
+        times.innerHTML=`00:${current}`;
+    }
 }
